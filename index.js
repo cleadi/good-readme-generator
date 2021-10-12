4210c6c8bb8839b8d5f1b5dbc2bfeb425ac207c7
Dillon Cleaver <cleadi@fastmail.com>
2021-10-12 09:10:35 -0500

    Added first code, added npm inquirer, linked two script files.

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-// TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
 const generateMarkdown = require("./utils/generateMarkdown");
 
-// TODO: Create an array of questions for user input
 const questions = [
   {
     type: "input",
@@ -32,41 +30,26 @@ const questions = [
   },
   {
     type: "input",
-    message: "How does licensing work?",
-    name: "license"
-  },
-  {
-    type: "input",
     message: "Did anyone else contribute?",
-    name: "contents"
+    name: "credits"
   },
   {
     type: "input",
-    message: "What kind of tests can you run?",
-    name: "tests"
+    message: "How is your project licensed?",
+    name: "license"
   },
 ];
 
-// create these sections: Description, Installation, Usage, Contributing, and Tests
+fs.writeFile("README.md", finalReadMe, err => {
+  err ? console.log("Oh no! Something went wrong 😵‍💫.") : console.log("Here's your README.md file!")
+})
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-  // use fs package to write the file
-}
-
-// TODO: Create a function to initialize app
 function init() {
   inquirer
   .prompt(questions)
   .then(response => {
-    const readMe = `
-    
-    `
-    // use the response data to build one big README file (as a string)
-    // THEN -->
     const finalReadMe = generateMarkdown(response);
   })
 }
 
-// Function call to initialize app
 init();
